42d4fe50c02b5a4a7376552ab53d6dd92ea1c8b0
sirily11 <32106111+sirily11@users.noreply.github.com>
2025-02-05 20:44:34 +0800

    feat: add squarebutton

--- apps/storybook/src/stories/Title/IntroTitle.stories.ts
+++ apps/storybook/src/stories/Title/IntroTitle.stories.ts
@@ -4,7 +4,7 @@ import { IntroTitleStory } from "./IntroTitle";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
-  title: "Ui/Intro/Title",
+  title: "Ui/Title/IntroTitle",
   component: IntroTitleStory,
   parameters: {
     // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
--- packages/ui/src/button.tsx
+++
@@ -1,3 +0,0 @@
-export const Button = () => {
-  return <button className={"bg-amber-200"}>Hello world</button>;
-};
